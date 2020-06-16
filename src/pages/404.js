56049bb0f5cc3f4c21aed45088b2404538417e6e
Joey Morello <joeymorello.one@gmail.com>
2020-06-16 16:07:16 -0500

    styled 404 page

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,16 @@
 import React from "react"
-
+import { Link } from 'gatsby';
 import Layout from "../components/layout"
 import SEO from "../components/seo"
+import "./404.scss"
 
 const NotFoundPage = () => (
   <Layout>
-    <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <SEO title="Whoops" />
+      <div className="copy__container">
+        <h1>404</h1>
+        <Link className="link--box-style" to='/'>Portfolio</Link>
+      </div>
   </Layout>
 )
 
