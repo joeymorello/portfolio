f0fd5a1e0a645fdd60912f2f034ae258c80921a6
Joey Morello <joeymorello.one@gmail.com>
2020-08-16 17:54:56 -0500

    changed home link to /, moved link to top left on mobile & fixed z-index issue

--- src/components/home-return/index.js
+++ src/components/home-return/index.js
@@ -6,9 +6,7 @@ import './home.scss';
 
 const HomeReturn = () => (
     <div className='home__container'>
-        <Link className='home__link' to='/#projects'>[<span>home</span>]</Link>
-        {/* <Link className='home__link home__link--next' to='/'>[prev]</Link>
-        <Link className='home__link home__link--next' to='/'>[next]</Link> */}
+        <Link className='home__link' to='/' >[<span>home</span>]</Link>
     </div>
 )
 
