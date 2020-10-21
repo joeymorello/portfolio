0ec611048a7b606fe31a09993495f98687d65e1a
Joey Morello <joeymorello.one@gmail.com>
2020-10-21 13:05:48 -0500

    updated insta link

--- src/components/accounts/index.js
+++ src/components/accounts/index.js
@@ -6,7 +6,7 @@ import './accounts.scss';
 const Accounts = () => (
     <ul className="accounts-list">
         <li><a href="https://www.linkedin.com/in/joey-morello" target='_blank' rel="noopener noreferrer">linked.</a></li>
-        <li><a href="https://www.instagram.com/itwasguy" target='_blank' rel="noopener noreferrer">insta.</a></li>
+        <li><a href="https://www.instagram.com/joeymmyeoj/" target='_blank' rel="noopener noreferrer">insta.</a></li>
         <li><a href="https://github.com/joeymorello" target='_blank' rel="noopener noreferrer">github.</a></li>
     </ul>
 )
